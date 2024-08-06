a9586400c69a4f1b08e652ffcbbe407be3422d43
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-06 15:45:17 -0400

    Complete code along

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,32 +1,28 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-  # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
 
-  # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
   end
 
-  # GET /birds/:id
   def show
     bird = find_bird
     render json: bird
   end
 
-  # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    bird.update!(bird_params)
     render json: bird
   end
 
-  # DELETE /birds/:id
   def destroy
     bird = find_bird
     bird.destroy
@@ -47,4 +43,7 @@ class BirdsController < ApplicationController
     render json: { error: "Bird not found" }, status: :not_found
   end
 
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
